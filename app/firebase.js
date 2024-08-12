7e477a108628aa816e014f56673e82481f86e47c
Omar Samiullah <78840499+osamiull@users.noreply.github.com>
2024-08-11 22:59:13 -0700

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
@@ -8,17 +8,18 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgYK1IT7xwrmR93t0sOerybFVhqH3faTk",
-  authDomain: "inventory-management-d9e7b.firebaseapp.com",
-  projectId: "inventory-management-d9e7b",
-  storageBucket: "inventory-management-d9e7b.appspot.com",
-  messagingSenderId: "733036480925",
-  appId: "1:733036480925:web:b01867a377f13ed990c774",
-  measurementId: "G-2GBRV5WH3H"
+  apiKey: "AIzaSyDxDoLA7HzAzVhEcABPRqsPntVlEeRLCfQ",
+  authDomain: "customer-support-47887.firebaseapp.com",
+  projectId: "customer-support-47887",
+  storageBucket: "customer-support-47887.appspot.com",
+  messagingSenderId: "510765943372",
+  appId: "1:510765943372:web:0b2b10c7ebf6a33e8742e9",
+  measurementId: "G-0TTNP6Y58D"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+// export default app;
